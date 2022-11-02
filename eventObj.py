fbbf82a345e575adea0648eddf103a0a2b22118f
atomdog <agom@bu.edu>
2022-11-02 14:58:51 -0400

    distracted

---
+++ eventObj.py
@@ -0,0 +1,29 @@
+
+class event:
+    def __init__(self):
+
+        #task; can occur @ any time(s) X before time y
+        #event; occurs @ time x for time y regardless
+        self.type = None
+
+        self.arrival = None
+        self.deadline = None
+
+        self.time_to_complete = None
+
+        self.time_completed = None
+
+        #actively being completed
+        self.active = None
+
+    #function to return time until deadline
+    def time_to_deadline(self, t):
+        return(0)
+    
+    #function to return time since arrival
+    def time_since_arrival(self, t):
+        return(0)
+
+    #return time to complete of time left until deadline
+    def haste(self, t):
+        return(0)
